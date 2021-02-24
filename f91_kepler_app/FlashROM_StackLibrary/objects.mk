9d0da30ded13d3d69a69b3bcdf73c83764e3d4de
Pegor <pegork@gmail.com>
2021-02-23 23:12:18 -0800

    Add ignore files

--- f91_kepler_app/FlashROM_StackLibrary/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"C:/ti/simplelink_cc2640r2_sdk_4_40_00_10/source/ti/devices/cc26x0r2/driverlib/bin/ccs/driverlib.lib" -l"C:/ti/simplelink_cc2640r2_sdk_4_40_00_10/kernel/tirtos/packages/ti/dpl/lib/dpl_cc26x0r2.aem3" -l"C:/ti/simplelink_cc2640r2_sdk_4_40_00_10/source/ti/drivers/lib/drivers_cc26x0r2.aem3" -l"C:/ti/simplelink_cc2640r2_sdk_4_40_00_10/source/ti/display/lib/display.aem3" -l"C:/ti/simplelink_cc2640r2_sdk_4_40_00_10/source/ti/grlib/lib/ccs/m3/grlib.a" -l"C:/Users/pegor/Desktop/Projects/f91-sw/f91-fw/f91_kepler_stack_library/FlashROM_Library/ble_r2.symbols" -l"C:/Users/pegor/Desktop/Projects/f91-sw/f91-fw/f91_kepler_stack_library/FlashROM_Library/lib_linker.cmd" -l"C:/Users/pegor/Desktop/Projects/f91-sw/f91-fw/f91_kepler_stack_library/FlashROM_Library/f91_kepler_stack_library.lib" -l"C:/ti/simplelink_cc2640r2_sdk_4_40_00_10/source/ti/blestack/common/cc26xx/ccs/cc26xx_app.cmd" -llibc.a
-
