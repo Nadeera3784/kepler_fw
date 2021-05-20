b1c2b987fee2ad81795845a980f66ae28f34e509
Pegor <pegork@gmail.com>
2021-05-19 17:49:58 -0700

    Initial phone app commit

---
+++ Software/app/src/main/java/com/example/f91_kepler/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.f91_kepler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
